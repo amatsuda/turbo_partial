37c1f6ef41358d98d262573b7db326a7465a63c1
Akira Matsuda <ronnie@dio.jp>
2019-07-04 08:12:46 +0900

    How about retrieving Template instance from ObjectSpace?

--- lib/turbo_partial.rb
+++ lib/turbo_partial.rb
@@ -1,47 +1,26 @@
 require "turbo_partial/version"
 
 module TurboPartial
-  class << self
-    # identifier_method_name
-    #"app_views_users_new_html_erb"
-    # @identifier
-    #"/Users/matsuda/tmp/render_include_tes/app/views/users/new.html.erb"
-    # inspect
-    #"app/views/users/new.html.erb"
+  module RelativeRenderer
+    def render_partial(context, options, &block)
+      partial = options[:partial]
 
-    def compute_method_name(path)
-      identifier_method_name = path.tr('^a-z_', '_')
+      if partial.start_with? './'
+        current_template = context.instance_variable_get :@current_template
+        current_path = current_template.identifier
+        partial_path = File.join(File.dirname(current_path), "_#{partial[2..-1]}#{current_path[/\..*/]}")
 
-      m = "_#{identifier_method_name}__#{Rails.root.join(path).to_s.hash}_"
-      m.tr!('-', '_')
-      m
-    end
-  end
-
-  module ActionView
-    module PartialRenderer
-      def render(context, options, block)
-        setup(context, options, block)
-
-        if @path.start_with? '/'
-          full_path = 'app/views' + @path
-        elsif @path.start_with? '.'
-          #TODO
+        if (partial_template = ObjectSpace.each_object(ActionView::Template).detect {|o| o.identifier == partial_path })
+          partial_template.render context, options[:locals]
         else
-          return super
-        end
-        full_path.sub!(/(\/)([^\/]*)$/, '/_\2')
-        method_name_prefix = TurboPartial.compute_method_name full_path
-        if (meth = context.methods.grep(/^#{method_name_prefix}/).first)
-          Rails.logger.debug 'turbo_rendering...'
-          context.public_send meth, options[:locals], context.output_buffer
-          nil
-        else
+          options[:partial] = partial[2..-1]
           super
         end
+      else
+        super
       end
     end
   end
 end
 
-::ActionView::PartialRenderer.prepend TurboPartial::ActionView::PartialRenderer
+ActionView::Renderer.prepend TurboPartial::RelativeRenderer
